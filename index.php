07796398ed28489666bb64b432efa226bc3da18d
Sabuj <mhsabuj1@gmail.com>
2020-10-19 14:24:30 +0600

    working code

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Barcode Generator</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div style="margin: 10%;">
+  	<h2 class="text-center">PHP BARCODE GENERATOR</h2>
+  	<form class="form-horizontal" method="post" action="barcode.php" target="_blank">
+  	<div class="form-group">
+      <label class="control-label col-sm-2" for="product">Product:</label>
+      <div class="col-sm-10">
+        <input autocomplete="OFF" type="text" class="form-control" id="product" name="product">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="product_id">Product ID:</label>
+      <div class="col-sm-10">
+        <input autocomplete="OFF" type="text" class="form-control" id="product_id" name="product_id" onkeyup="show()">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="rate">Rate</label>
+      <div class="col-sm-10">          
+        <input autocomplete="OFF" type="text" class="form-control" id="rate"  name="rate">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="print_qty">Barcode Quantity</label>
+      <div class="col-sm-10">          
+        <input autocomplete="OFF" type="print_qty" class="form-control" id="print_qty"  name="print_qty">
+      </div>
+    </div>
+
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+  </div>
+</div>
+
+</body>
+</html>
+
+<script>
+let data = ''
+
+window.onload = function () {
+     window.document.body.addEventListener('keydown', function(event){
+        if( event.keyCode == 13 || event.keyCode == 16 ||  event.keyCode == 17 ) {
+                event.preventDefault();
+                    return;
+                }
+
+                if(event.ctrlKey) {
+                    event.preventDefault();
+                    return;
+                }
+
+        /*data += event.key
+        console.log(data)*/
+    });
+}
+</script>
+
+<script type="text/javascript">
+  function show(){
+    console.log($('#product_id').val());
+  }
+</script>
